c50b73cc766738bf0e96d3713041294a8b453df1
chloejay <ji.jie@edhec.com>
2019-09-05 00:15:46 +0800

    sample

--- chapter3_pyFunc/error.py
+++ chapter3_pyFunc/error.py
@@ -1,4 +1,5 @@
 #exception is everywhere in Python, for if something you are not sure, you can use the exception to make it through 
+#error handling code and error checking,  Programs sometimes fail, especially during development, and if you can’t determine why failures occur, you’re flying blind.
 
 def my_generator():
     try:
@@ -10,4 +11,18 @@ def my_generator():
         print ("ok let's clean") 
 
 
+def mergeList(listA, listB):
+    new_list=list() 
+    a= 0
+    b=0 
+
+    while a <len(listA) and b< len(listB):
+        if listA[a]<listB[b]:
+            new_list.appen(listA)
+            a+=1 
+        else:
+            new_list.appen(listB)
+            b+=1 
+            
+    return new_list 
 
